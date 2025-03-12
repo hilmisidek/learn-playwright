b59a73a3208f319baa17cbd3dffb7470e3fc91b4
hilmisidek <khairulhilmi.sidek@petronas.com>
2025-03-12 13:16:29 +1100

    add pom

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,38 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+import {HomePage} from '../pages/home-page';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+const URL = 'https://playwright.dev/';
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+let homePage : HomePage;
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test.beforeEach(async ({page})=>{
+  await page.goto(URL);
+  homePage = new HomePage(page);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page).toHaveURL(/.*intro/);
-});
+})
+
+async function clickGetStarted(){
+  await homePage.clickGetStarted();
+  //await page.getByRole('link', { name: 'Get started' }).click();
+}
+
+test.describe('Playwright website', () =>{
+    
+  test('has title', async () => {
+      // Expect a title "to contain" a substring.
+      await homePage.assertPageTitle();
+    });
+
+    test('get started link', async ({ page }) => {
+      // Click the get started link.
+      await clickGetStarted();
+
+      // Expects page to have a heading with the name of Installation.
+      await expect(page).toHaveURL(/.*intro/);
+    });
+
+})
+
+
+
